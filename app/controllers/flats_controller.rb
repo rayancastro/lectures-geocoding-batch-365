9ff7bd9d5235943192fe5ce04e816ee6299427af
Rayan Castro <rayancdc@gmail.com>
2020-02-26 10:47:50 +0100

    add nice plugins to our map

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,7 +7,9 @@ class FlatsController < ApplicationController
     @markers = @flats.map do |flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { flat: flat }),
+        image_url: helpers.asset_url('weird_dog.jpeg')
       }
     end
   end
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,14 +1,54 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+const buildMap = () => {
+  return (
+    new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/rayancastro/cjw99aj4q11j51cm5q1ibork0'
+    })
+  )
+}
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 140, maxZoom: 15, duration: 5000 });
+}
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+  const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+  // Create a HTML element for your custom marker
+  const element = document.createElement('div');
+  element.className = 'marker';
+  element.style.backgroundImage = `url('${marker.image_url}')`;
+  element.style.backgroundSize = 'contain';
+  element.style.width = '25px';
+  element.style.height = '25px';
+
+  new mapboxgl.Marker(element)
+    .setLngLat([ marker.lng, marker.lat ])
+    .setPopup(popup)
+    .addTo(map);
+  });
+}
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
-    });
+    const markers = JSON.parse(mapElement.dataset.markers);
+
+
+    const map = buildMap();
+    addMarkersToMap(map, markers);
+    fitMapToMarkers(map, markers);
+
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                      mapboxgl: mapboxgl }));
   }
 };
 
